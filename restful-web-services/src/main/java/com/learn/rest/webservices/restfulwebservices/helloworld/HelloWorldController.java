95c3fa388aa0e9d42ec11a66a5bfe6fb4b263bd7
Sahana Tavari <tavarisahana10@gmail.com>
2024-02-15 15:43:00 +0530

    first project on springboot

---
+++ restful-web-services/src/main/java/com/learn/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -0,0 +1,24 @@
+package com.learn.rest.webservices.restfulwebservices.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	@GetMapping(path = "/hello-world")
+	public String helloworld() {
+		return "Hello World";
+	}
+	
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World"); 
+	}
+
+	@GetMapping(path = "/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldBeanPathVariable(@PathVariable String name) {
+		return new HelloWorldBean("Hello World " + name);
+	}
+}
---
+++ restful-web-services/src/main/java/com/learn/rest/webservices/restfulwebservices/users/UserNotFoundException.java
@@ -0,0 +1,13 @@
+package com.learn.rest.webservices.restfulwebservices.users;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
+public class UserNotFoundException extends RuntimeException {
+	
+	public UserNotFoundException(String message) {
+		super(message);
+	}
+
+}
